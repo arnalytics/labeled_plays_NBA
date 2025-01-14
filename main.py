3dbbd0124f461d490a31093f2b4b437c8f26fa3b
arnaubarrera <arnau6baroy@gmail.com>
2025-01-15 00:18:28 +0100

    we are able to get the links and labels ina DF!!

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pandas as pd
+import bs4 as BeautifulSoup
+import os
 import requests
-import bs4 as bs 
+import concurrent.futures
 
-# Script to download videos once the URLs are obtained.
-# The problem is that getting the URLs is not straightforward, the web crawler is in the notebook.
 
-# The URL has the following form:
-'...media/YEAR/MONTH/DAY/PLAYER_ID/'
-# Video URL
-video_url = "https://videos.nba.com/nba/pbp/media/2024/11/12/0022400005/20/7ac52325-8749-0f24-fee9-c90ad6c85274_1280x720.mp4"
+# Accept cookies
+def accept_cookies(driver):
+    try:
+        cookie_accept_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))
+        )
+        cookie_accept_button.click()  # Click on 'Accept'
+        print("Cookies banner closed.")
+    except Exception as e:
+        print("No cookies banner found or other issue:", e)
 
-# Download the video file
-response = requests.get(video_url, stream=True)
 
-# Save the file
-with open("nba_video.mp4", "wb") as file:
-    for chunk in response.iter_content(chunk_size=8192):
-        file.write(chunk)
-
-print("Video downloaded successfully!")
+def clean_missing_video_data(df: pd.DataFrame):
+    """
+    Elimina las filas que tienen el vídeo missing.
+    """
+    # Filtrar las filas donde 'Video Link' no es igual a la URL de video faltante
+    df_cleaned = df[df['Video Link'] != 'https://videos.nba.com/nba/static/missing.mp4']
+    
+    return df_cleaned
