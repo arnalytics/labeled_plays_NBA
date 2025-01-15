b554a28cfd5e12b36d6880199e5c1a35245d1975
arnaubarrera <arnau6baroy@gmail.com>
2025-01-15 14:03:52 +0100

    Going back to general table and selecting another player implemented. Save labels in csv after iteration of each player for safety.

--- main.py
+++ main.py
@@ -9,6 +9,39 @@ import bs4 as BeautifulSoup
 import os
 import requests
 import concurrent.futures
+import re
+
+
+def initialize_driver(season: str='2023-24'):
+
+    if season != ['2024-25', '2023-24', '2022-23', '2021-22', '2020-21', '2019-20', '2018-19', '2017-18', 
+                  '2016-17', '2015-16']:
+        raise Exception("Not available video-data for this season")
+
+    # Initialize the Chrome driver
+    driver = webdriver.Chrome()
+
+    # Navigate to the player statistics page
+    driver.get("https://www.nba.com/stats/players/traditional")
+
+    # Explicit wait to ensure the table is loaded
+    time.sleep(3)
+
+    accept_cookies(driver)
+    time.sleep(3)
+
+    # Select season
+    dropdown_season_xpath = '//*[@id="__next"]/div[2]/div[2]/div[3]/section[1]/div/div/div[1]/label/div/select'
+    dropdown_element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, dropdown_season_xpath))
+    )
+    select = Select(dropdown_element)
+    select.select_by_value(season)
+
+    # Explicit wait to ensure the table is loaded
+    time.sleep(3)
+
+    return driver
 
 
 # Accept cookies
@@ -31,3 +64,68 @@ def clean_missing_video_data(df: pd.DataFrame):
     df_cleaned = df[df['Video Link'] != 'https://videos.nba.com/nba/static/missing.mp4']
     
     return df_cleaned
+
+
+def count_rows_table_plays(driver, table_xpath: str):
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, table_xpath))
+        )
+        
+        # Encuentra todas las filas de la tabla
+        rows = driver.find_elements(By.XPATH, f"{table_xpath}/tr")
+        
+        # Cuenta el número de filas
+        rows_number = len(rows)
+    except Exception as e:
+        rows_number = 45
+        print(f"Error al contar las filas: {e}")
+
+    return rows_number
+
+
+def extract_assist_info(play_description: str):
+    # Patrón para capturar el nombre del jugador sin el número antes de "AST"
+    pattern = r"\(([^0-9()]+) \d+ AST\)"
+    
+    # Buscar el patrón en la cadena
+    match = re.search(pattern, play_description)
+    
+    if match:
+        # Extraer el nombre del jugador antes de "AST"
+        player_name = match.group(1).strip()
+        return True, player_name  # AST presente con nombre del jugador
+    else:
+        return False, None  # No hay AST
+
+
+def create_dataframe(df_type: str = 'FGA'):
+    """
+    Crea un DataFrame con las columnas apropiadas según el tipo de estadísticas proporcionado.
+    
+    df_type: str
+        Tipo de estadística para determinar las columnas del DataFrame. 
+        Ejemplo: ['FGA', 'REB', 'BLK']
+    """
+
+    # Diccionario con las columnas por tipo
+    columns_by_type = {
+        'FGA': [
+            ' ', 'PLAYER', 'PLAY TYPE', 'MADE', 'SHOT TYPE', 'BOXSCORE', 'VTM', 'HTM', 'GAME DATE',
+            'PERIOD', 'TIME REMAINING', 'SHOT DISTANCE (FT)', 'TEAM', 'AST', 'Assisted by', 'Video Link'
+        ],
+        'REB': [
+            ' ', 'PLAYER', 'REBOUND TYPE', 'BOXSCORE', 'VTM', 'HTM', 'GAME DATE', 'PERIOD', 'Video Link'
+        ],
+        'BLK': [
+            ' ', 'PLAYER', 'BOXSCORE', 'VTM', 'HTM', 'GAME DATE', 'PERIOD', 'Video Link'
+        ],
+        'STL/TOV': [
+            ' ', 'PLAYER', 'BOXSCORE', 'VTM', 'HTM', 'GAME DATE', 'PERIOD', 'Video Link'
+        ]
+    }
+
+    columns = columns_by_type.get(df_type)
+    labels_df = pd.DataFrame(columns=columns)
+
+    return labels_df
